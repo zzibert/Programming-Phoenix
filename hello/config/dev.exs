cef5ab0d32ef1d862cb13c6e3cc7cce6121d7d72
zan <zan_zibert@hotmail.com>
2020-05-11 19:25:42 +0200

    adding a hello world route

--- hello/config/dev.exs
+++ hello/config/dev.exs
@@ -3,8 +3,8 @@ use Mix.Config
 # Configure your database
 config :hello, Hello.Repo,
   username: "postgres",
-  password: "postgres",
-  database: "hello_dev",
+  password: "nekineki",
+  database: "postgres",
   hostname: "localhost",
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
