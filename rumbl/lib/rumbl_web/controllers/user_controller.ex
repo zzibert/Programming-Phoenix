b907583e191de00c4541ec55d2c965e14aff4917
zan <zan_zibert@hotmail.com>
2020-05-16 18:12:45 +0200

    template for creating user form

--- rumbl/lib/rumbl_web/controllers/user_controller.ex
+++ rumbl/lib/rumbl_web/controllers/user_controller.ex
@@ -3,6 +3,8 @@ defmodule RumblWeb.UserController do
 
   alias Rumbl.Accounts
 
+  alias Rumbl.Accounts.User
+
   def index(conn, _params) do
     users = Accounts.list_users()
     render(conn, "index.html", users: users)
