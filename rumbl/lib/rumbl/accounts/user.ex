42012119f3bf9577a5173fa3502e16744aee9a85
zan <zan_zibert@hotmail.com>
2020-05-17 15:18:12 +0200

    adding password to users

--- rumbl/lib/rumbl/accounts/user.ex
+++ rumbl/lib/rumbl/accounts/user.ex
@@ -5,6 +5,8 @@ defmodule Rumbl.Accounts.User do
   schema "users" do
     field :name, :string
     field :username, :string
+    field :password, :string, virtual: true
+    field :password_hash, :string
 
     timestamps()
   end
@@ -14,5 +16,14 @@ defmodule Rumbl.Accounts.User do
     |> cast(attrs, [:name, :username])
     |> validate_required([:name, :username])
     |> validate_length(:username, min: 1, max: 20)
+  end
+
+  def registration_changeset(user, params) do
+    user
+    |> changeset(params)
+    |> cast(params, [:password])
+    |> validate_required([:password])
+    |> validate_length(:password, min: 6, max: 100)
+    |> put_pass_hash()
   end
 end
