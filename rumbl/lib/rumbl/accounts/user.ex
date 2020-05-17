d21e04dbd523744782e3a0113b088b689d7f5af3
zan <zan_zibert@hotmail.com>
2020-05-17 15:24:18 +0200

    adding put_pass_hash functions

--- rumbl/lib/rumbl/accounts/user.ex
+++ rumbl/lib/rumbl/accounts/user.ex
@@ -26,4 +26,13 @@ defmodule Rumbl.Accounts.User do
     |> validate_length(:password, min: 6, max: 100)
     |> put_pass_hash()
   end
+
+  defp put_pass_hash(changeset) do
+    case changeset do
+      %Ecto.Changeset{valid?: true, changes: %{password: pass}} ->
+        put_change(changeset, :password_hash, Pbkdf2.hash_pwd_salt(pass))
+      _ ->
+        changeset
+    end
+  end
 end
